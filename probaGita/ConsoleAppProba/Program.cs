0c688d58c93d754495e59590b51b49402f7273c6
Milan992 <mitic.gsm@gmail.com>
2020-06-15 09:59:52 +0200

    micin prvi komit

--- probaGita/ConsoleAppProba/Program.cs
+++ probaGita/ConsoleAppProba/Program.cs
@@ -10,13 +10,7 @@ namespace ConsoleAppProba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("bilo sta");
-            Console.WriteLine("nesto drugo bilo sta");
-            Console.WriteLine("treca");
-            Console.WriteLine("treca");
-            Console.WriteLine("bilo sta");
-            Console.WriteLine("nesto drugo bilo sta");
-            Console.WriteLine("treca");
+            Console.WriteLine("moja grana");
         }
     }
 }
